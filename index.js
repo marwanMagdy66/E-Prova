bfd9b702c916eee03456d5249b22c743a1e0ecf3
marwanMagdy66 <marwanmagdy826@gmail.com>
2025-02-20 18:36:34 +0200

    updates

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL || "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+
 app.use(
   cors({
     origin: [process.env.BASE_URL || "http://localhost:3000"],
