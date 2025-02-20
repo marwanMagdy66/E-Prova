87b4e13075863dc7143585814464f6e070790332
marwanMagdy66 <marwanmagdy826@gmail.com>
2025-02-20 18:58:15 +0200

    updates

--- index.js
+++ index.js
@@ -12,10 +12,20 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-
+  const allowedOrigins = [
+    "http://localhost:3000", 
+    "http://localhost:5173", // لدعم Vite
+    process.env.CLIENT_URL || "https://e-prova.vercel.app"
+  ];
+  
 app.use((req, res, next) => {
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL || "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
@@ -26,10 +36,10 @@ app.use((req, res, next) => {
   next();
 });
 
-
+// إضافة `cors()` كـ Middleware احتياطي
 app.use(
   cors({
-    origin: [process.env.BASE_URL || "http://localhost:3000"],
+    origin: allowedOrigins,
     credentials: true,
   })
 );
